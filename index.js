9abcda91681d63fde4a44be693e6fcde3cdd31dd
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2020-12-13 01:59:44 +0530

    old api reverted

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const RedditScraper = require("reddit-scraper");
-const axios = require("axios");
 
 // const port = process.env.PORT || 5000;
 const app = express();
@@ -42,49 +41,23 @@ app.get("/ser/:date/:ser/", (req, res) => {
   const ser = req.params.ser;
   const date = req.params.date;
   var dates = { h: "qdr:h", d: "qdr:d", w: "qdr:w", m: "qdr:m", y: "qdr:y" };
-  console.log(dates[date]);
   var opts = {
     searchTerm: decodeURIComponent(ser),
     queryStringAddition: `&tbs=ic:trans&tbs=${dates[date]}&safe=active`,
   };
-  var data = [];
+
   gis(opts, logResults);
   function logResults(error, results) {
     if (error) {
       console.log(error);
     } else {
-      var getresult = Promise.all(
-        results.forEach((element) => {
-          axios
-            .head(element["url"])
-            .then((result) => {
-              data.push(element);
-              console.log(result.status);
-            })
-            .catch((err) => {
-              // console.log(err.response.status);
-            });
-        })
-      );
-
-      //   if (condition) {
-      //   axios.get()
-      // }
-      if (data.length != 0) {
-      }
-      getresult
-        .then((result) => {
-          res.status(200).json({
-            data,
-          });
-          console.log(data, result);
-        })
-        .catch((err) => {});
-
+      res.status(200).json({
+        results,
+      });
       // console.log(JSON.stringify(results, null, "  "));
     }
   }
-  console.log(decodeURIComponent(ser));
+  // console.log(decodeURIComponent(ser));
 });
 var server = app.listen(8081, function () {
   //   var host = server.address().address;
@@ -93,10 +66,4 @@ var server = app.listen(8081, function () {
   console.log("Example app listening at http://localhost:" + port);
 });
 
-axios
-  .get(
-    "http://chatbotapi-env.eba-hrayjeem.us-east-2.elasticbeanstalk.com/rj/atharvachatbot/chat?query=lolo"
-  )
-  .then(function (response) {
-    console.log(response);
-  });
+
